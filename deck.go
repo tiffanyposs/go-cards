396637e3a7354db9217602c89ea93aea9f5ddedf
Tiffany Poss <tiffanyposs@gmail.com>
2021-02-04 23:04:33 -0500

    create newDeck function

--- deck.go
+++ deck.go
@@ -5,9 +5,26 @@ import "fmt"
 // Create a new type of deck
 // which is a slice of strings
 
-// create a new type called deck that
+// create a new type called "deck" that
 // extends the functionality of a slice of strings
 type deck []string
+
+// create a new function that returns a type "deck", which we created above
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	// if there is a variable you don't need to use, replace it with an underscore
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
 
 // any variable with the type deck, gets a method named print
 // (d deck) syntax is called a receiver
@@ -17,7 +34,7 @@ type deck []string
 // by convention, the first variable in a receiver is named with 1 or 2 letters that match
 // the first letters of the type it's extended, this is why we named this "d" instead of "cards"
 func (d deck) print() {
-	for i, card := range d {
-		fmt.Println(i, card)
+	for _, card := range d {
+		fmt.Println(card)
 	}
 }
--- main.go
+++ main.go
@@ -1,13 +1,8 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
-
+	cards := newDeck()
 	cards.print()
 }
 
-// you must declare what type this function with return
-func newCard() string {
-	return "Five of Diamonds"
-}
+// $ go run main.go deck.go
